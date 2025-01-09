5200b1723f459961e471b2086f2bbe7352fea038
matthewrodrigues <matthew.rodrigues@berkeley.edu>
2025-01-09 15:31:26 -0800

    initial commit

--- Python1.2.py
+++ Python1.2.py
@@ -6,14 +6,13 @@ from langdetect import detect, LangDetectException
 
 app = Flask(__name__)
 
-# Your credentials
+# Credentials
 client_id = 'cc226c7567e645d0a811e66cf214128f'
 client_secret = '155a2ad652984092a8ecba0357237093'
 redirect_uri = 'http://localhost:4000/callback'
 
 
-print("zoom")
-# Step 1: Redirect user to Spotify's authorization page
+# Redirect user to Spotify's authorization page
 @app.route('/login')
 def login():
     scopes = 'playlist-modify-public playlist-modify-private'
@@ -21,7 +20,6 @@ def login():
         f'https://accounts.spotify.com/authorize?response_type=code&client_id={client_id}'
         f'&scope={scopes}&redirect_uri={redirect_uri}'
     )
-    print("go")
     return redirect(auth_url)
 
 # Step 2: Handle the callback from Spotify
@@ -35,6 +33,12 @@ def callback():
     ask = str(input("What's the name of the playlist you want to modify:"))
     ask4 = str(input("Do you want to remove or keep songs(choose r or k):"))
     ask2 = str(input("What type of language songs do you want to remove/keep from the playlist (English(en), Spanish(es), Hindi(hi), or Japanese(ja)):"))
+    ask5 = int(input("Please give a starting index of song, as Spotify API is limited to parsing through 100 songs(this will go through the 100 songs including the song you choose), if you just want to go through your first hundred songs please enter 99999:"))
+    if ask5 == 99999:
+        j = 0
+    else:
+        j = ask5 - (ask5%100)
+    #checks through all playlists and selects playlist based on playlist name
     for i in range(0,len(playlists["items"])):
         if ask == (playlists['items'][i]["name"]):
             playlist_id = playlists['items'][i]["id"]
@@ -43,13 +47,12 @@ def callback():
     playlist_id_new = create_playlist_get_id(access_token, user_id, ask3, public = True)
     print(ask2)
     print(new_playlist)
-    #j = 1
-    # For simplicity, let's use the first playlist
     if playlists['items']:
         print(playlist_id)
-        tracks = get_playlist_tracks(playlist_id, access_token)
+        tracks = get_playlist_tracks(playlist_id, access_token, offset = j)# starts from "j - j%100" song in playlist
+        print(tracks)
         songs = []
-        for track in tracks:
+        for track in tracks:# iterates through 100 songs at most in playlist
             print(f"Track: {track['name']}") 
             print(f"Artist: {track['artists'][0]['name']}")
             trackname = f"{track['name']}"
@@ -72,7 +75,6 @@ def callback():
      #       if j >= 110:
      #           break
             print()
-    print(songs)
     add_tracks_to_playlist(access_token, playlist_id_new, songs)
     #add a keep songs of only a certain language feature too
     #
@@ -111,12 +113,27 @@ def get_user_playlists(access_token):
     print("worked")
     return response.json()
 
-def get_playlist_tracks(playlist_id, access_token):
+def get_playlist_tracks(playlist_id, access_token, offset = 0):
     url = f'https://api.spotify.com/v1/playlists/{playlist_id}/tracks'
     headers = {'Authorization': f'Bearer {access_token}'}
-    response = requests.get(url, headers=headers)
-    tracks = response.json()['items']
-    return [track['track'] for track in tracks]
+    data = {
+        "offset": offset
+            }
+    
+    response = requests.get(url, headers=headers, params=data) 
+    tracks_data = response.json()['items'] 
+    tracks = []
+
+    for song in tracks_data:
+        tracks.append(song["track"])
+    print(tracks)
+   # for track in tracks:
+   #     print(track["name"])
+    
+    return tracks
+
+
+
 
 # Example: Create a new playlist 
 def create_playlist(access_token, user_id, name): 
